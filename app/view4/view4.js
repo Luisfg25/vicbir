58da71d58ea34ab4d9c2b9f81afcab6c9f2157fb
wa05 <waltercejas19@hotmail.com>
2016-04-05 02:08:01 -0300

    Working on Gallery

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -5,13 +5,27 @@ angular.module('myApp.view4', [])
 .controller('View4Ctrl', ['$scope',function($scope) {
 
 	$scope.images=[
-		{"name": 1,"location":"imgs/1.jpg", "id":0},
-		{"name": 2,"location":"imgs/2.jpg", "id":1},
-		{"name": 3,"location":"imgs/3.jpg", "id":2},
-		{"name": 4,"location":"imgs/4.jpg", "id":3},
-		{"name": 5,"location":"imgs/5.jpg", "id":4},
-		{"name": 6,"location":"imgs/6.jpg", "id":5},
-		{"name": 7,"location":"imgs/7.jpg", "id":6}
+		{"name": "Cantando","location":"imgs/1.jpg", "id":0},
+		{"name": "Tocando","location":"imgs/2.jpg", "id":1},
+		{"name": "Ensayo","location":"imgs/3.jpg", "id":2},
+		{"name": "Ensayo","location":"imgs/4.jpg", "id":3},
+		{"name": "Ensayo","location":"imgs/5.jpg", "id":4},
+		{"name": "Recital","location":"imgs/6.jpg", "id":5},
+		{"name": "Recital","location":"imgs/7.jpg", "id":6}
 	]	
+
+	$scope.myInterval = 10000;
+    $scope.noWrapSlides = false;
+    var currIndex = 0;
+    
+
+    $scope.hola = false;
+    $scope.activar = function(currentPic){
+    	$scope.active = currentPic;	
+    	$scope.hola = true;
+    };
+
+    
+
 
 }]);

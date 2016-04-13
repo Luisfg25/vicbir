902ede244ed23ffe131d109594d1fe3b8d4d5c08
unknown <lui.s91@hotmail.com>
2016-04-12 21:26:53 -0300

    view2

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,6 +2,20 @@
 
 angular.module('myApp.view2', [])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', [function($scope) {
+	$scope.notice=[
+	{
+		"title":"titulo1",
+		"imagen":"",
+		"descripcion":"desc1",
+	},{
+		"title":"tutylo2",
+		"imagen":"",
+		"descripcion":"desc2",
+	},{
+		"title":"titulo3",
+		"imagen":"",
+		"descripcion":"desc3",
+	}
+	]
 }]);
